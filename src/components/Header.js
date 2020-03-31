b93ca89b8f3cc571b9c9b96f4dec5b8c2539b3fb
TanimaRanjan <tanimaranjan@gmail.com>
2020-03-31 15:26:54 -0400

    Header and Loader

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => (
+    <header className='header'>
+        <div className='content-container'>
+            <div className='header__content' >
+                <h1 className='header__title'>MOBILIZE</h1>
+            </div>
+        </div>
+    </header>
+)
+
+export default Header
